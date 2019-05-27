202ef562fa3f48c1ccfb760574e3cd1c264abeb1
Florin H <hugentoblerf@mail.ch>
2019-05-27 13:25:25 +0200

    updated footer

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -3,7 +3,22 @@
     <p>Alle Rechte vorbehalten, evenements©, 2019</p>
   </section>
   <section>
-    <a href="<?php $url ?>/impressum/"></a>
+    <article class="links">
+      <a href="<?php $url ?>/home/">Home</a>
+      <a href="<?php $url ?>/impressum/">Impressum</a>
+      <a href="<?php $url ?>/login/">Login</a>
+      <a href="<?php $url ?>/newproject/">Projekt erstellen</a>
+    </article>
+    <article class="address">
+      <p>Evenement AG</p>
+      <p>Bernweg 11</p>
+      <p>3000 Bern</p>
+      <p>Switzerland</p>
+    </article>
+    <article class="contact">
+      <a href="mailto:info@evenement.ch">info@evenement.ch</a>
+      <a href="tel:+41011212232">+41 01 121 22 32</a>
+    </article>
   </section>
 </footer>
 </html>
