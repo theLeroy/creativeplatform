62822915f1a32d1c0b2b6446efd358eddc4bdea1
Leroy <4c65726f79@gmail.com>
2019-06-03 14:00:28 +0200

    change

--- components/neededcontainer/neededcontainer.php
+++ components/neededcontainer/neededcontainer.php
@@ -6,36 +6,35 @@ function neededcontainer() {
       <p class="neededpers_pay"><span class=neededpers_titl>Wir bieten</span> 20.- pro std.</p>
     </div>
 
-    <style media="screen">
-
-    .neededpers {
-      background: rgba(230, 0, 227, 0.5);
-      border-radius: 4px;
-      display: inline-block;
-      margin-right: 10px;
-      width: 300px;
-      height: 100px;
-    }
-    .neededpers > .neededpers_titl {
-      font-weight: bold;
-    }
-    .neededpers > .neededpers_Search, .neededpers_pay {
-      color: #fff;
-      display: block;
-      position: absolute;
-      height: 30px;
-      text-align: center;
-      width: 300px;
-    }
-    .neededpers > .neededpers_Search {
-      margin-top: 20px;
-    }
-    .neededpers > .neededpers_pay {
-      margin-top: 60px;
-    }
-    </style>
-
   ';
-
 }
  ?>
+
+     <style media="screen">
+
+     .neededpers {
+       background: rgba(230, 0, 227, 0.5);
+       border-radius: 4px;
+       display: inline-block;
+       margin-right: 10px;
+       width: 300px;
+       height: 100px;
+     }
+     .neededpers > .neededpers_titl {
+       font-weight: bold;
+     }
+     .neededpers > .neededpers_Search, .neededpers_pay {
+       color: #fff;
+       display: block;
+       position: absolute;
+       height: 30px;
+       text-align: center;
+       width: 300px;
+     }
+     .neededpers > .neededpers_Search {
+       margin-top: 20px;
+     }
+     .neededpers > .neededpers_pay {
+       margin-top: 60px;
+     }
+     </style>
