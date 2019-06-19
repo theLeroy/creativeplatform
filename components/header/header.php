d8d904d8cfc7106da24f1b4c427e45783f87122d
Leroy <4c65726f79@gmail.com>
2019-06-19 17:39:35 +0200

    added add

--- components/header/header.php
+++ components/header/header.php
@@ -18,15 +18,27 @@
 
   </head>
   <header class="mainheader">
+
     <div class="headerlogo">
       <img id="headerImage" src="<?php $url ?>/media/Icon/Logo.svg" alt="">
-    </div>
-    <div class="headeradd">
-      add
     </div>
     <div class="headeruserpicture">
       <img src="/media/Bilder/users/user_id_1/mainimg.jpg" class="" alt="">
     </div>
+    <a href="/pages/newproject">
+    <div class="headeradd">
+      <?xml version="1.0" encoding="iso-8859-1"?>
+      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      	 viewBox="0 0 52 52" style="enable-background:new 0 0 52 52;" xml:space="preserve">
+      <g>
+      	<path d="M26,0C11.664,0,0,11.663,0,26s11.664,26,26,26s26-11.663,26-26S40.336,0,26,0z M26,50C12.767,50,2,39.233,2,26
+      		S12.767,2,26,2s24,10.767,24,24S39.233,50,26,50z"/>
+      	<path d="M38.5,25H27V14c0-0.553-0.448-1-1-1s-1,0.447-1,1v11H13.5c-0.552,0-1,0.447-1,1s0.448,1,1,1H25v12c0,0.553,0.448,1,1,1
+      		s1-0.447,1-1V27h11.5c0.552,0,1-0.447,1-1S39.052,25,38.5,25z"/>
+      </g>
+      </svg>
+    </div>
+  </a>
   </header>
 
   <style media="screen">
@@ -58,6 +70,19 @@
     }
     .headeradd {
       display: inline-block;
+      float: right;
+      margin-top: 25px;
+      margin-left: 20px;
+      margin-right: 20px;
+      cursor: pointer;
+    }
+    .headeradd > a {
+      width: 50px;
+      height: 50px;
+    }
+    .headeradd > svg {
+      width: 50px;
+      height: 50px;
     }
     .headeruserpicture {
       display: inline-block;
